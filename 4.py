d32745d7975b6fb92b08733cbc2d6ee2088fd6b6
Aiyyskhan <timirkhan@gmail.com>
2019-09-30 15:33:10 +0900

    add test python files

---
+++ 4.py
@@ -0,0 +1,10 @@
+a = ['a', 'b', 'c', 'd', 'e', 'f']
+b = []
+
+for i in a[:3]:
+	b.append(i)
+
+import arcade
+import numpy as np
+
+print('test')
---
+++ 5.py
@@ -0,0 +1,13 @@
+# a = int(input('enter: '))
+# b = int(input('enter: '))
+
+# print(a == b)
+
+a = [1,2,3,4]
+
+def mod(lst):
+	lst[2] = 100
+
+mod(a)
+
+print(a)
